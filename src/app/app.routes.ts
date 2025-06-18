621d4ac109da2f5f0f52df2727f1cb8988c7d4e1
Romane Duchant <romane@macbookair.home>
2025-06-18 13:05:29 -0400

    first-elements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MentionsLegalesComponent } from './pages/mentions-legales/mentions-legales.component';
+import { DonneesPersonnellesComponent } from './pages/donnees-personnelles/donnees-personnelles.component';
+import { AccessibiliteComponent } from './pages/accessibilite/accessibilite.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { PolitiqueCookiesComponent } from './pages/politique-cookies/politique-cookies.component';
+import { GestionCookiesComponent } from './pages/gestion-cookies/gestion-cookies.component'
+import { BatimentComponent } from './pages/batiment/batiment.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { FabricationComponent } from './pages/fabrication/fabrication.component';
+import { AlimentationComponent } from './pages/alimentation/alimentation.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'batiment', component: BatimentComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'fabrication', component: FabricationComponent },
+    { path: 'alimentation', component: AlimentationComponent },
+    { path: 'mentions-legales', component: MentionsLegalesComponent },
+    { path: 'donnees-personnelles', component: DonneesPersonnellesComponent },
+    { path: 'accessibilite', component: AccessibiliteComponent },
+    { path: 'contacts', component: ContactsComponent },
+    { path: 'politique-cookies', component: PolitiqueCookiesComponent },
+    { path: 'gestion-cookies', component: GestionCookiesComponent },
+
+];
---
+++ src/app/pages/politique-cookies/politique-cookies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-politique-cookies',
+  imports: [],
+  templateUrl: './politique-cookies.component.html',
+  styleUrl: './politique-cookies.component.scss'
+})
+export class PolitiqueCookiesComponent {
+
+}
